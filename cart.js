b3047c82e9b641a876c89e9ee386ddb6105049fd
porky3n <s10255872@connect.np.edu.sg>
2023-11-30 16:10:18 +0800

    Refined Whole Cart JS for Cart to Store Items in Local Storage

--- cart.js
+++ cart.js
@@ -1,129 +1,155 @@
 /*Cart*/
 
 window.addEventListener('load', function () {
-    //Event listener for clicking cart icon//
-    document.getElementById('cart-icon').addEventListener('click', function () {
-      var cartPopup = document.getElementById('cart-popup');
-      //Toggle display of cart pop-up//
-      cartPopup.style.display = (cartPopup.style.display === 'block') ? 'none' : 'block';
-    });
-
-    //Event listener for clicking checkout button//
-    document.getElementById('checkout-button').addEventListener('click', function () {
-        var totalSpan = document.getElementById('total-price');
-        var totalPrice = parseFloat(totalSpan.textContent);
-    
-        //If cart is empty when press checkout button, print fail message//
-        if (totalPrice === 0) {
-          displayErrorMessage("Purchase Failed. Your Cart is Empty.");
-        } else {
-          //Print purchase success message//
-          var message = `Thank You For Your Purchase! Payment Instructions Sent to Email.`;
-          displayPurchaseMessage(message);
-          clearShoppingCart();
-      }
-      });
-
-      //Function to display purchase success message//
-      function displayPurchaseMessage(message) {
-        var purchaseMessage = document.getElementById('product-purchase-message');
-        purchaseMessage.textContent = message;
-        purchaseMessage.style.display = 'block';
-    
-        //Message to disappear after 2s//
-        setTimeout(function () {
-          purchaseMessage.style.display = 'none';
-        }, 2000);
-      }
-
-      //Function to display purchase fail message//
-      function displayErrorMessage(message) {
-        var errorMessage = document.getElementById('fail-purchase-message');
-        errorMessage.textContent = message;
-        errorMessage.style.display = 'block';
-
-        //Message to disappear after 2s//
-        setTimeout(function () {
-            errorMessage.style.display = 'none';
-        },2000);
-    }
-    
-    // Get elements with the class 'add-to-cart-button'
-    var addToCartButtons = document.getElementsByClassName('add-to-cart-button');
-  
-    // Iterate through each 'Add to Cart' button and add an event listener
-    Array.from(addToCartButtons).forEach(function (button) {
-      button.addEventListener('click', function () {
-        var itemName = button.getAttribute('data-ball-name');
-        var itemPrice = parseFloat(button.getAttribute('data-ball-price'));
-        
-        // Add the item to the cart
-        addToCart(itemName, 1, itemPrice);
-
-        // Change button text temporarily to 'Item Added to Cart'
-        button.textContent = 'Item Added to Cart';
-        
-        //Reset button text after 1.5s//
-        setTimeout(function () {
-          button.textContent = 'Add to Cart';
-        }, 1500);
-      });
-    });
-  
-    //Function to add items to cart//
-    function addToCart(itemName, quantity, price) {
-      var cartItems = document.getElementById('cart-items');
-      var totalSpan = document.getElementById('total-price');
-  
-      //Check if item already in cart//
-      var existingItem = Array.from(cartItems.children).find(function (item) {
-        return item.dataset.name === itemName;
-      });
-  
-      if (existingItem) {
-        
-        var currentQuantity = parseInt(existingItem.dataset.quantity);
-        var newQuantity = currentQuantity + quantity;
-
-        //If item already in cart, only increase quantity count and price accordingly//
-        existingItem.dataset.quantity = newQuantity;
-        existingItem.cells[1].textContent = newQuantity;
-        existingItem.cells[2].textContent = '$' + (newQuantity * price).toFixed(2);
-      } else {
-        
-        //If item not in cart, create a new row in the table//
-        var newRow = cartItems.insertRow();
-        newRow.dataset.name = itemName;
-        newRow.dataset.quantity = quantity;
-        
-        //Add item details (name,quantity,price) to each cell in the row//
-        var cell1 = newRow.insertCell(0);
-        cell1.textContent = itemName;
-  
-        var cell2 = newRow.insertCell(1);
-        cell2.textContent = quantity;
-  
-        var cell3 = newRow.insertCell(2);
-        cell3.textContent = '$' + (quantity * price).toFixed(2);
-      }
-  
-      // Update the total price//
-      var currentTotal = parseFloat(totalSpan.textContent);
-      totalSpan.textContent = (currentTotal + price).toFixed(2);
-    }
-
-    //Function to clear shopping cart//
-    function clearShoppingCart() {
-      var cartItems = document.getElementById('cart-items');
-      var totalSpan = document.getElementById('total-price');
-
-      cartItems.innerHTML = ''; 
-      // Clear all rows from the cart//
-      totalSpan.textContent = '0.00'; 
-      // Reset total price to zero//
-  }
+  // Event listener for clicking cart icon
+  document.getElementById('cart-icon').addEventListener('click', function () {
+    var cartPopup = document.getElementById('cart-popup');
+    // Toggle display of cart pop-up
+    cartPopup.style.display = (cartPopup.style.display === 'block') ? 'none' : 'block';
   });
+
+  // Event listener for clicking checkout button
+  document.getElementById('checkout-button').addEventListener('click', function () {
+    var totalSpan = document.getElementById('total-price');
+    var totalPrice = parseFloat(totalSpan.textContent);
+
+    if (totalPrice === 0) {
+      displayErrorMessage("Purchase Failed. Your Cart is Empty.");
+    } else {
+      var message = `Thank You For Your Purchase! Payment Instructions Sent to Email.`;
+      displayPurchaseMessage(message);
+      clearShoppingCart();
+    }
+  });
+
+  // Function to display purchase success message
+  function displayPurchaseMessage(message) {
+    var purchaseMessage = document.getElementById('product-purchase-message');
+    purchaseMessage.textContent = message;
+    purchaseMessage.style.display = 'block';
+
+    // Message to disappear after 2s
+    setTimeout(function () {
+      purchaseMessage.style.display = 'none';
+    }, 2000);
+  }
+
+  // Function to display purchase fail message
+  function displayErrorMessage(message) {
+    var errorMessage = document.getElementById('fail-purchase-message');
+    errorMessage.textContent = message;
+    errorMessage.style.display = 'block';
+
+    // Message to disappear after 2s
+    setTimeout(function () {
+      errorMessage.style.display = 'none';
+    }, 2000);
+  }
+
+  // Get elements with the class 'add-to-cart-button'
+  var addToCartButtons = document.getElementsByClassName('add-to-cart-button');
+
+  // Iterate through each 'Add to Cart' button and add an event listener
+  Array.from(addToCartButtons).forEach(function (button) {
+    button.addEventListener('click', function () {
+      var itemName = button.getAttribute('data-ball-name');
+      var itemPrice = parseFloat(button.getAttribute('data-ball-price'));
+
+      // Add the item to the cart
+      addToCart(itemName, 1, itemPrice);
+
+      // Change button text temporarily to 'Item Added to Cart'
+      button.textContent = 'Item Added to Cart';
+
+      // Reset button text after 1.5s
+      setTimeout(function () {
+        button.textContent = 'Add to Cart';
+      }, 1500);
+    });
+  });
+
+  // Function to add items to cart and local storage
+  function addToCart(itemName, quantity, price) {
+    var cartItems = document.getElementById('cart-items');
+    var totalSpan = document.getElementById('total-price');
+
+    // Check if item already in cart
+    var existingItem = Array.from(cartItems.children).find(function (item) {
+      return item.dataset.name === itemName;
+    });
+
+    if (existingItem) {
+      var currentQuantity = parseInt(existingItem.dataset.quantity);
+      var newQuantity = currentQuantity + quantity;
+
+      // If item already in cart, only increase quantity count and price accordingly
+      existingItem.dataset.quantity = newQuantity;
+      existingItem.cells[1].textContent = newQuantity;
+      existingItem.cells[2].textContent = '$' + (newQuantity * price).toFixed(2);
+    } else {
+      // If item not in cart, create a new row in the table
+      var newRow = cartItems.insertRow();
+      newRow.dataset.name = itemName;
+      newRow.dataset.quantity = quantity;
+
+      // Add item details (name,quantity,price) to each cell in the row
+      var cell1 = newRow.insertCell(0);
+      cell1.textContent = itemName;
+
+      var cell2 = newRow.insertCell(1);
+      cell2.textContent = quantity;
+
+      var cell3 = newRow.insertCell(2);
+      cell3.textContent = '$' + (quantity * price).toFixed(2);
+    }
+
+    // Update the total price
+    var currentTotal = parseFloat(totalSpan.textContent);
+    totalSpan.textContent = (currentTotal + price).toFixed(2);
+
+    // Store cart items in local storage
+    updateLocalStorage();
+  }
+
+  // Function to update local storage with current cart items
+  function updateLocalStorage() {
+    var cartItems = document.getElementById('cart-items');
+    var cartData = [];
   
+    Array.from(cartItems.children).forEach(function (item) {
+      cartData.push({
+        name: item.dataset.name,
+        quantity: parseInt(item.dataset.quantity),
+        price: parseFloat(item.cells[2].textContent.replace('$', '')) / parseInt(item.dataset.quantity) // Fix here
+      });
+    });
+  
+    localStorage.setItem('cart', JSON.stringify(cartData));
+  }
+
+
+  // Function to clear shopping cart and local storage
+  function clearShoppingCart() {
+    var cartItems = document.getElementById('cart-items');
+    var totalSpan = document.getElementById('total-price');
+
+    cartItems.innerHTML = '';
+    totalSpan.textContent = '0.00';
+
+    // Clear local storage
+    localStorage.removeItem('cart');
+  }
+
+  // On page load, check if there are items in local storage and add them to the cart
+  var storedCart = localStorage.getItem('cart');
+  if (storedCart) {
+    var cartData = JSON.parse(storedCart);
+    cartData.forEach(function (item) {
+      addToCart(item.name, item.quantity, item.price);
+    });
+  }
+});
+
 
 /*Close Cart*/
 document.addEventListener('DOMContentLoaded', function () {
